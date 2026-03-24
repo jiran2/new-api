9589f31607448416901cdd0f81558de729053741
jiran888 <jiran888@163.com>
2026-03-24 15:32:27 +0800

    Change session cookie MaxAge to -1 for no expiration

--- main.go
+++ main.go
@@ -172,7 +172,7 @@ func main() {
 	store := cookie.NewStore([]byte(common.SessionSecret))
 	store.Options(sessions.Options{
 		Path:     "/",
-		MaxAge:   2592000, // 30 days
+		MaxAge:   -1, // 30 days
 		HttpOnly: true,
 		Secure:   false,
 		SameSite: http.SameSiteStrictMode,
